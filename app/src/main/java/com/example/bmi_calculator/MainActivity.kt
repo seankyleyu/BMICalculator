42eae77ac1f8fb7a1515c7d6ade06a51425dcc05
Sean Kyle Yu <seankyleyu@gmail.com>
2019-08-28 20:29:29 +0800

    Added functionality

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.bmi_calculator
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
-import android.widget.Toast.LENGTH_LONG
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val weightTypes = arrayOf("kg","lb")
         val heightTypes = arrayOf("cm","ft")
+
         var weightT = "kg"
         var heightT = "cm"
 
@@ -47,7 +47,73 @@ class MainActivity : AppCompatActivity() {
 
         calculate.setOnClickListener{
 
+            if(height.length() > 0 && weight.length() > 0) {
 
+                var w = weight.text.toString().toDouble()
+                var h = height.text.toString().toDouble()
+                var bmi = 0.0
+
+                heightType.visibility = View.GONE
+                weightType.visibility = View.GONE
+                height.visibility = View.GONE
+                weight.visibility = View.GONE
+                calculate.visibility = View.GONE
+
+                back.visibility = View.VISIBLE
+                bmiLabel.visibility = View.VISIBLE
+                bmiVal.visibility = View.VISIBLE
+
+                if(weightT == "lb"){
+
+                    //pounds to kilograms
+                    w *= 0.453592
+
+                }
+
+                if(heightT == "ft"){
+
+                    //feet to centimeters
+                    h *= 30.48
+
+                }
+
+                //centimeters to meters
+                h *= 0.01
+
+                bmi = w / (h * h)
+
+                //bmiVal.text = bmi.toString()
+                bmiVal.text = "%.1f".format(bmi)
+
+                if(bmi < 18.5){
+                    //Underweight
+                    bmiVal.setTextColor(Color.parseColor("#fff033"))
+                }else if(bmi < 24.9){
+                    //Normal weight
+                    bmiVal.setTextColor(Color.parseColor("#74ff33"))
+                }else if(bmi < 29.9){
+                    //Overweight
+                    bmiVal.setTextColor(Color.parseColor("#ffa833"))
+                }else{
+                    //Obesity
+                    bmiVal.setTextColor(Color.parseColor("#ff3333"))
+                }
+
+            }
+
+        }
+
+        back.setOnClickListener{
+
+            heightType.visibility = View.VISIBLE
+            weightType.visibility = View.VISIBLE
+            height.visibility = View.VISIBLE
+            weight.visibility = View.VISIBLE
+            calculate.visibility = View.VISIBLE
+
+            back.visibility = View.GONE
+            bmiLabel.visibility = View.GONE
+            bmiVal.visibility = View.GONE
 
         }
     }
